745bfd636dfc294fd0d57c52c4368f523102448f
Mohamed Hafez <mhafez1978@outlook.com>
2023-09-24 02:25:36 -0400

    netlify test3

--- www/server/app/contact/page.js
+++ www/server/app/contact/page.js
@@ -397,7 +397,7 @@ const PageHeader = ({ pageTitle })=>{
 var __webpack_require__ = require("../../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [587,802,526], () => (__webpack_exec__(4182)));
+var __webpack_exports__ = __webpack_require__.X(0, [587,802,647,979], () => (__webpack_exec__(4182)));
 module.exports = __webpack_exports__;
 
 })();
